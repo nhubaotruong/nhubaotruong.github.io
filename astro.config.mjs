6dce1b2e6b79a15ba605e05483a52d3b3a69d6e5
nhubao <baoprokute1234@gmail.com>
2023-04-02 22:47:12 +0700

    Change avatar, Update icon, change color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()], 
+  integrations: [preact(), purgecss()],
   site: "https://nhubaotruong.github.io"
 });
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,34 @@
 const Main = () => {
     const contactInfos = [
-        { logo: "/github.svg", link: "https://github.com/nhubaotruong/" },
-        { logo: "/linkedin.png", link: "https://www.linkedin.com/in/nhubaotruong/" }
+        { logo: <i class="fa-brands fa-github fa-xl" style="color: #fff;" alt="github" />, link: "https://github.com/nhubaotruong/" },
+        { logo: <i class="fa-brands fa-linkedin fa-xl" style="color: #fff;" alt="linkedin" />, link: "https://www.linkedin.com/in/nhubaotruong/" },
+        { logo: <i class="fa-solid fa-inbox fa-xl" style="color: #fff;" alt="gmail" />, link: "mailto:baoprokute1234@gmail.com" },
     ]
     return (
         <div>
-            <div class="justify-content-center align-items-center d-flex">
-                <img src="/avatar.jpg" alt="logo" class="img-fluid rounded-circle" style="height: 15em" />
-            </div>
-            <h1 class="text-center text-white py-4 fw-semibold">Trương Như Bảo</h1>
-            <h3 class="text-center text-white">Backend Engineer (you can tell from the css)</h3>
-            <h3 class="text-center text-white">GNU/Linux user and gamer (Arch BTW)</h3>
-            <h3 class="text-center text-white" onClick={() => window.open(atob("aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ=="), "_blank")}>sometimes a Rick Roller</h3>
+            <h1 class="justify-content-center align-items-center d-flex">
+                <img src="/avatar.webp" alt="avatar" class="img-fluid rounded-circle" style="height: 7.5em" />
+            </h1>
+            <h1 class="text-center py-4 fw-semibold">Trương Như Bảo</h1>
+            <h3 class="text-center">Backend Engineer (you can tell from the css)</h3>
+            <h3 class="text-center">GNU/Linux user and gamer (Arch BTW)</h3>
+            <h3 class="text-center" onClick={() => window.open(atob("aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ=="), "_blank")}>sometimes a Rick Roller</h3>
 
-            <h1 class="text-center text-white mt-5">
+            <h1 class="text-center mt-5 fw-semibold">
                 Contacts:
             </h1>
-            <h2 class="text-center text-white">
+            <h2 class="text-center d-flex justify-content-center mt-4">
                 {
                     contactInfos.map(({ logo, link }) => (
-                        <a href={link} target='_blank'><img src={logo} class="img-fluid mx-3" alt={logo} style="height: 2em" /></a>
+                        <h1 class="mx-2">
+                            <a href={link} target='_blank'>
+                                {logo}
+                            </a>
+                        </h1>
                     ))
                 }
             </h2>
-        </div>
+        </div >
     )
 }
 
