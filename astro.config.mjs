caa407ad089f37e6df5947cfb089170d9971dba6
nhubao <baoprokute1234@gmail.com>
2023-04-04 18:04:12 +0700

    Use critters to inline css, avoid weird stutter at first load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import purgecss from "astro-purgecss";
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), purgecss()],
+  integrations: [preact(), purgecss(), critters({ pruneSource: true, preload: "swap", inlineFonts: true })],
   site: "https://nhubaotruong.github.io"
 });
