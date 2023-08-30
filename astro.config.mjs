27f67a4dd9b14ee16b4e604791dcdf84a72c8cf6
nhubao <baoprokute1234@gmail.com>
2023-08-30 14:34:47 +0700

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import purgecss from "astro-purgecss";
 import compress from "astro-compress";
-
 import vue from "@astrojs/vue";
+
+import critters from "astro-critters";
+
+const crittersConfig = critters({
+  pruneSource: true,
+  preload: "body",
+  inlineFonts: true,
+  path: "./dist",
+  publicPath: "/",
+})
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), purgecss(), compress()],
+  integrations: [vue(), purgecss(), compress(), crittersConfig],
   site: "https://nhubaotruong.github.io"
 });
