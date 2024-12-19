38e6f04d0b6f77b43d0657dd02cc0d78a56f5345
nhubao <baoprokute1234@gmail.com>
2024-12-20 00:49:29 +0700

    Cooler new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig } from "astro/config";
-import purgecss from "astro-purgecss";
 import compress from "astro-compress";
 import critters from "astro-critters";
+import tailwind from "@astrojs/tailwind";
 
 const crittersConfig = critters({
-  pruneSource: true,
-  preload: "body",
-  inlineFonts: true,
-  path: "./dist",
-  publicPath: "/",
-})
+	pruneSource: true,
+	preload: "body",
+	inlineFonts: true,
+	path: "./dist",
+	publicPath: "/",
+});
+
+const getSiteUrl = () => {
+	// Cloudflare Pages sets CF_PAGES=1 during build
+	if (process.env.CF_PAGES) {
+		// CF_PAGES_URL is available in production, CLOUDFLARE_DEV_HOSTNAME in preview
+		return (
+			process.env.CF_PAGES_URL ||
+			`https://${process.env.CLOUDFLARE_DEV_HOSTNAME}`
+		);
+	}
+	return "https://nhubaotruong.github.io"; // Default to GitHub Pages
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss(), compress(), crittersConfig],
-  site: "https://nhubaotruong.github.io"
+	integrations: [tailwind(), compress(), crittersConfig],
+	site: getSiteUrl(),
 });
