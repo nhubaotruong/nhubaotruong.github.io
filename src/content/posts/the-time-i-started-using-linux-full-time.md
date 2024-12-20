b2165fa677982ad35e1c6e6b9e1625be1cf5419b
nhubao <baoprokute1234@gmail.com>
2024-12-20 20:10:13 +0700

    Redesigned my site

--- src/content/posts/the-time-i-started-using-linux-full-time.md
+++
@@ -1,9 +0,0 @@
----
-id: 1
-title: The time I started using Linux full time
-author: Nhu Bao Truong
-date: 2023-04-15
-tags: ["linux", "windows"]
----
-
-Hi
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      scale: {
-        '102': '1.02',
-      },
-      animation: {
-        'fadeIn': 'fadeIn 0.5s ease forwards',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'system-ui', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 } 
