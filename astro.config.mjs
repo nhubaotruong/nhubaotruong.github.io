1b9d1e3780c8d6dec98b2fb24dbe31dc8b73c012
nhubao <baoprokute1234@gmail.com>
2025-02-03 09:58:40 +0700

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,27 @@ import critters from "astro-critters";
 import tailwind from "@astrojs/tailwind";
 
 const crittersConfig = critters({
-	pruneSource: true,
-	preload: "body",
-	inlineFonts: true,
-	path: "./dist",
-	publicPath: "/",
+    pruneSource: true,
+    preload: "body",
+    inlineFonts: true,
+    path: "./dist",
+    publicPath: "/",
 });
 
 const getSiteUrl = () => {
-	// Cloudflare Pages sets CF_PAGES=1 during build
-	if (process.env.CF_PAGES) {
-		// CF_PAGES_URL is available in production, CLOUDFLARE_DEV_HOSTNAME in preview
-		return (
-			process.env.CF_PAGES_URL ||
-			`https://${process.env.CLOUDFLARE_DEV_HOSTNAME}`
-		);
-	}
-	return "https://nhubaotruong.github.io"; // Default to GitHub Pages
+    // Cloudflare Pages sets CF_PAGES=1 during build
+    if (process.env.CF_PAGES) {
+        // CF_PAGES_URL is available in production, CLOUDFLARE_DEV_HOSTNAME in preview
+        return (
+            process.env.CF_PAGES_URL ||
+            `https://${process.env.CLOUDFLARE_DEV_HOSTNAME}`
+        );
+    }
+    return "https://nhubaotruong.github.io"; // Default to GitHub Pages
 };
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), compress(), crittersConfig],
-	site: getSiteUrl(),
+    integrations: [tailwind(), compress(), crittersConfig],
+    site: getSiteUrl(),
 });
